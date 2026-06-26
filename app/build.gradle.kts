4c4d52aa1c03adeeae35163cee92788b04747477
XaClaviiier <angelfongiman@gmail.com>
2026-06-27 01:16:00 +0700

    fix dashboard tracker habbit, sambungin spinner combobox, inisialisasi list" icon untuk habit, nambahin sharedpreferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.fragment)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
